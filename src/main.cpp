50a85db23efdd888ca222911efa54e4dc62161f8
Samik Ganguly <samik.ganguly@outlook.com>
2020-12-25 13:34:54 +0530

    Empty winmain

---
+++ src/main.cpp
@@ -0,0 +1,13 @@
+#ifndef UNICODE
+#define UNICODE
+#endif
+
+#ifndef _UNICODE
+#define _UNICODE
+#endif
+
+#include <windows.h>
+
+int WINAPI wWinMain(HINSTANCE hInstance, HINSTANCE, PWSTR pCmdLine, int nCmdShow) {
+	return 0;
+}
